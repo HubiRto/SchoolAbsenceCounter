6f9e26a6283e650078882f8fae7974e618fdd0d7
hubert <hubert.rybicki.hr@gmail.com>
2023-05-25 10:50:55 +0200

    Dodanie adnotacji @Builder do prostszego tworzenia obiektów.

--- src/main/java/pl/pomoku/schoolabsencecounter/entity/User.java
+++ src/main/java/pl/pomoku/schoolabsencecounter/entity/User.java
@@ -2,6 +2,7 @@ package pl.pomoku.schoolabsencecounter.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Data
 @Entity
+@Builder
 @Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
