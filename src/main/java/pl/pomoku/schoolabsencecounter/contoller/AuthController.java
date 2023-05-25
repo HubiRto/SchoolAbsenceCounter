fb1c49dcad719043763b9df9133b57fadf342498
hubert <hubert.rybicki.hr@gmail.com>
2023-05-25 10:54:50 +0200

    Dodanie do kontrolera ścieżki /register

--- src/main/java/pl/pomoku/schoolabsencecounter/contoller/AuthController.java
+++ src/main/java/pl/pomoku/schoolabsencecounter/contoller/AuthController.java
@@ -1,12 +1,21 @@
 package pl.pomoku.schoolabsencecounter.contoller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.pomoku.schoolabsencecounter.dto.UserDto;
 
 @Controller
 public class AuthController {
     @GetMapping("/index")
     public String home(){
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
     }
 }
